0918514633dea9b57ff593f21c084ec741ce53ff
Kunalnagarkoti <123114443+Kunalnagarkoti@users.noreply.github.com>
2023-04-29 09:03:13 +0530

    Update countdown.js

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "April 13, 2023 10:00:00",
+    date: "May 4, 2023 10:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
